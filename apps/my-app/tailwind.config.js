6bd737a690a4a654722a4dc80f3a149971e6292e
Mateusz Jabłoński <mail@mateuszjablonski.com>
2025-11-22 14:36:39 +0100

    task 4

---
+++ apps/my-app/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./renderer.js",
+  ],
+  darkMode: 'class',
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
